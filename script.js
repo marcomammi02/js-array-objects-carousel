53169f047c677271a948f4807745ae2278e39430
marcomammi02 <marcomammi2002@gmail.com>
2022-11-08 18:52:53 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const arrImages = [
+	{
+		image: '01.webp',
+		title: "Marvel's Spiderman Miles Morale",
+		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+	},
+	{
+		image: '02.webp',
+		title: 'Ratchet & Clank: Rift Apart',
+		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+	},
+	{
+		image: '03.webp',
+		title: 'Fortnite',
+		text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+	},
+	{
+		image: '04.webp',
+		title: 'Stray',
+		text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+	},
+	{
+		image: '05.webp',
+		title: "Marvel's Avengers",
+		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+	},
+];
+
+const eleSliderContainer = document.querySelector('.slider-container');
+const eleSmallImgContainer = document.querySelector('.small-img-container');
+
+for (let i = 0; i < arrImages.length; i++) {
+    const eleImg = document.createElement('img');
+    eleImg.src = `img/${arrImages[i].image}`;
+    eleImg.classList.add('slider-img');
+    eleSmallImgContainer.innerHTML += `<img class="small-img" src="${arrImages[i]}">`;
+    const eleSmallImg = document.querySelector('.small-img');
+
+    if (i == 0) {
+        eleImg.classList.add('active');
+        eleSmallImg.classList.add('active-small');
+    }
+
+    eleSliderContainer.append(eleImg);
+}
+
+const eleBtnPrev = document.querySelector('.btn-prev');
+const eleBtnNext = document.querySelector('.btn-next');
+const eleImgList = document.querySelectorAll('.slider-img');
+const eleSmallImgList = document.querySelectorAll('.small-img')
+
+let activeIndex = 0
+
+eleBtnNext.addEventListener('click', function() {
+    if (activeIndex < eleImgList.length) {
+        eleImgList[activeIndex].classList.remove('active');
+        eleSmallImgList[activeIndex].classList.remove('active-small');
+
+        activeIndex++;
+
+        if (activeIndex == eleImgList.length) {
+            activeIndex = 0;
+        } 
+
+        eleImgList[activeIndex].classList.add('active');
+        eleSmallImgList[activeIndex].classList.add('active-small');
+
+    }
+})
+eleBtnPrev.addEventListener('click', function() {
+    if (activeIndex >= 0) {
+        eleImgList[activeIndex].classList.remove('active');
+        eleSmallImgList[activeIndex].classList.remove('active-small');
+
+        if (activeIndex == 0) {
+            activeIndex = eleImgList.length - 1;
+        } else {
+            activeIndex--;
+        }
+
+        eleImgList[activeIndex].classList.add('active');
+        eleSmallImgList[activeIndex].classList.add('active-small');
+
+    }
+})
